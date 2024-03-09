379544dc56a7f0d5aae9c341e8069ce465420984
wxfeiang <mac@wangpengdeMacBook-Pro-2017.local>
2024-03-09 22:05:41 +0800

    feat: init

---
+++ src/views/Login/components/index.ts
@@ -0,0 +1,10 @@
+import FogetPasswordForm from './FogetPasswordForm.vue'
+import LoginForm from './LoginForm.vue'
+import LoginFormTitle from './LoginFormTitle.vue'
+import MobileForm from './MobileForm.vue'
+import QrCodeForm from './QrCodeForm.vue'
+import RegisterForm from './RegisterForm.vue'
+import SSOLoginVue from './SSOLogin.vue'
+
+export { FogetPasswordForm, LoginForm, LoginFormTitle, MobileForm, QrCodeForm, RegisterForm, SSOLoginVue }
+
